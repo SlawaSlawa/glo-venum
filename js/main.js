3910cacf8b28fbf1a1f6c2198b9b877fe811a5ce
Slawa <slawapocht4@yandex.ru>
2021-06-11 16:44:23 +0400

    day 4

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-// 'use strict';
+'use strict';
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.nav-menu');
+const menuCloseBtn = document.querySelector('.menu-close');
+
+menuButton.addEventListener('click', () => {
+    menu.classList.add('is-active');
+    menuCloseBtn.classList.add('is-active');
+});
+
+menuCloseBtn.addEventListener('click', () => {
+    menu.classList.remove('is-active');
+    menuCloseBtn.classList.remove('is-active');
+});
+
+$('[data-fancybox]').fancybox({
+    thumbs: {
+        autoStart: true
+    }
+});
